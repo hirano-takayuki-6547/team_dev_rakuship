9a03a79c19463f475bd64f19642b8138424e924c
syu-yucing-7328 <429-student020@la-study.com>
2024-05-20 09:55:32 +0900

    カテゴリシータ記入済み

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,7 +14,18 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
-
+        $category=[
+            ['name'=>'ファッション'],
+            ['name'=>'家具・インテリア'],
+            ['name'=>'CD・DVD・Blu-ray'],
+            ['name'=>'楽器・アート'],
+            ['name'=>'生活家電'],
+            ['name'=>'キッチン・日常用品'],
+            ['name'=>'本・雑誌・漫画'],
+            ['name'=>'ペット用品'],
+            ['name'=>'アウトドア・旅行用品'],
+            ['name'=>'ベビー・キッズ・おもちゃ'],
+        ];
+        \DB::table('categories')->insert($category);
     }
 }
