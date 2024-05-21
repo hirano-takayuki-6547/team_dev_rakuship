d352a5b224c57a29f4ebfc922a539022d9a48576
syu-yucing-7328 <429-student020@la-study.com>
2024-05-21 09:59:10 +0900

    bougt-itemsビュー作成済み

---
+++ resources/views/mypage/bought-items.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAKUSHIP</title>
+</head>
+<body>
+        <h3>注文情報</h3>
+        <table border="1">
+            <thead>
+                <tr><th>ID</th><th>商品名</th><th>注文日</th></tr>
+            </thead>
+            <tbody>
+                @foreach($items as $item)
+                    <tr>
+                        <td>{{ $item->category_id }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->bought_at }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+</body>
+</html>
