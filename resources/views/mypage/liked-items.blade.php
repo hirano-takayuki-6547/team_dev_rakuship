76044b23abe1c7e78811d7f03743a7a6357aa3fb
syu-yucing-7328 <429-student020@la-study.com>
2024-05-21 13:14:56 +0900

    お気に入りビュー再作成

--- resources/views/mypage/liked-items.blade.php
+++ resources/views/mypage/liked-items.blade.php
@@ -1,7 +1,22 @@
-@extends('layouts.app')
-
 @section('content-mypage')
 <h1>お気に入り一覧</h1>
+<table border="1">
+    <thead>
+        <tr><th>ID</th><th>商品名</th><th>注文日</th></tr>
 
+    </thead>
+    <tbody>
+        @foreach($items as $item)
+
+            <tr>
+                <td>{{ $like->id }}</td>
+                <td>{{ $like->name }}</td>
+                <td>{{ $like->updated_at }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
 
 @endsection
