5ae2c24c88c346a840ff2c91036db37267e29885
syu-yucing-7328 <429-student020@la-study.com>
2024-05-21 16:35:16 +0900

    $likeを$itemに修正

--- resources/views/mypage/liked-items.blade.php
+++ resources/views/mypage/liked-items.blade.php
@@ -4,16 +4,15 @@
 <h3>お気に入り一覧</h3>
 <table border="1">
     <thead>
-        <tr><th>ID</th><th>商品名</th><th>注文日</th></tr>
+        <tr><th>ID</th><th>商品名</th></tr>
 
     </thead>
     <tbody>
         @foreach($items as $item)
 
             <tr>
-                <td>{{ $like->id }}</td>
-                <td>{{ $like->name }}</td>
-                <td>{{ $like->updated_at }}</td>
+                <td>{{ $item->id }}</td>
+                <td>{{ $item->name }}</td>
             </tr>
         @endforeach
     </tbody>
