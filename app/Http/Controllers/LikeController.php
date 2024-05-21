84f4141f22240231f62c6007339d87f7afa248f5
nishimura-ryo-7700 <429-student012@la-study.com>
2024-05-21 09:45:45 +0900

    LikeController の作成。

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class LikeController extends Controller
 {
-    //
+    // いいね登録
+    public function store(Request $request)
+    {
+        Auth::user()->likedItems()->attach($request->item_id);
+        return back();
+    }
+
+    // いいね解除
+    public function destroy(Request $request)
+    {
+        Auth::user()->likedItems()->detach($request->item_id);
+        return back();
+    }
 }
