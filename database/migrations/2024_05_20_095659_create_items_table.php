6840b7afcfc8cbf800061fdd0c49f4613066effa
hirano-takayuki-6547 <429-student005@la-study.com>
2024-05-22 14:35:13 +0900

    fixed mypage.index.blade.php

--- database/migrations/2024_05_20_095659_create_items_table.php
+++ database/migrations/2024_05_20_095659_create_items_table.php
@@ -33,8 +33,6 @@ return new class extends Migration
             $table->foreign('category_id')->references('id')
                 ->on('categories');
 
-
-
         });
     }
 
--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -1,48 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div>
-    <img src="" alt="商品画像">
-</div>
-<h3>商品名：{{ $item->name }}</h3>
-<h4>カテゴリ：{{ $item->category->name }}</h4>
-<p>価格：{{ $item->price }}円</p>
-<p>商品説明：{{ $item->description }}</p>
-<p>
-    @if ($item->user_id == Auth::id())
-        <a href="{{ route('item.edit', $item) }}">編集する</a>
-        |
-        <a href="" onclick="deleteitem()">削除する</a>
-        <form action="{{ route('item.destroy', $item)}}" method="post" id="delete-form" novalidate>
-            @csrf
-        </form>
-        <script type="text/javascript">
-            function deleteitem() {
-                event.preventDefault();
-                if (window.confirm('本当に削除しますか？')) {
-                    document.getElementById('delete-form').submit();
-                }
-            }
-        </script>
-    @else
-        @if (Auth::user()->isLike($item->id))
-            <form action="{{ route('likes.destroy') }}" method="post" novalidate>
-             @csrf
-             @method('delete')
-             <input type="hidden" name="item_id" value="{{ $item->id }}">
-             <button type="submit">いいね解除</button>
-            </form>
-        @else
-            <form action="{{ route('likes.store') }}" method="post" novalidate>
-                @csrf
-                <input type="hidden" name="item_id" value="{{ $item->id }}">
-                <button type="submit">いいね登録</button>
-            </form>
-        @endif
-        |
-        <a href="{{ route('show.buy.form', $item) }}">購入する</a>
+    @if (session('message'))
+        <div>
+            <span
+                style="color: 2f2b32; background-color: #d1e7dd; border-radius: 8px; padding: 4px 8px;">{{ session('message') }}
+            </span>
+        </div>
     @endif
-</p>
+    <div>
+        <img src="" alt="商品画像">
+    </div>
+    <h3>商品名：{{ $item->name }}</h3>
+    <h4>カテゴリ：{{ $item->category->name }}</h4>
+    <p>価格：{{ $item->price }}円</p>
+    <p>商品説明：{{ $item->description }}</p>
+    <p>
+        @if ($item->seller_id == Auth::id())
+            <a href="{{ route('item.edit', $item) }}">編集する</a>
+            |
+            <a href="" onclick="deleteitem()">削除する</a>
+            <form action="{{ route('item.destroy', $item->id) }}" method="post" id="delete-form">
+                @csrf
+            </form>
+            <script type="text/javascript">
+                function deleteitem() {
+                    event.preventDefault();
+                    if (window.confirm('本当に削除しますか？')) {
+                        document.getElementById('delete-form').submit();
+                    }
+                }
+            </script>
+        @else
+            @if (Auth::user()->isLike($item->id))
+                <form action="{{ route('likes.destroy') }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <input type="hidden" name="item_id" value="{{ $item->id }}">
+                    <button type="submit">いいね解除</button>
+                </form>
+            @else
+                <form action="{{ route('likes.store') }}" method="post">
+                    @csrf
+                    <input type="hidden" name="item_id" value="{{ $item->id }}">
+                    <button type="submit">いいね登録</button>
+                </form>
+            @endif
+            |
+            <a href="{{ route('show.buy.form', $item) }}">購入する</a>
+        @endif
+    </p>
 
 @endsection
