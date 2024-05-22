d98cf323279efa4675caedf50ace8e24c40e0eb1
hirano-takayuki-6547 <429-student005@la-study.com>
2024-05-22 16:37:27 +0900

    いろんなやつ

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -3,22 +3,14 @@
 @section('content')
     @foreach ($items as $item)
         <article class="card">
-            <div class="card__header">
-                <figure class="card__thumbnail">
-                    <img src="/storage/item_images/{{ $item->img_src }}" alt="商品画像" class="card__image">
-                </figure>
-                <h2 class="card__title">{{ $item->name }}</h2>
-            </div>
-            <div class="card__body">
-                <p class="card__description">{{ $item->description }}</p>
-                <p class="card__category">{{ $item->category->name }}</p>
-                <p class="card__category">{{ $item->itemCondition->name }}</p>
-                <p class="card__price">{{ $item->name }}</p>
-            </div>
-            <div class="card__footer">
-                <p class="card__text"><a href="{{ route('items.show', $item) }}"
-                        class="button -compact">{{ $item->name }}の詳細を見る</a></p>
-            </div>
+            <a href="{{ route('items.show', $item->id) }}" style="text-decoration: none; color: #000000;">
+                <div class="card__header">
+                    <figure class="card__thumbnail">
+                        <img src="/storage/item_images/{{ $item->img_src }}" alt="商品画像" class="card__image" style="object-fit: cover; width: 300px; height: 300px;">
+                    </figure>
+                    <h2 class="card__title">{{ $item->name }}</h2>
+                </div>
+            </a>
         </article>
     @endforeach
     {{ $items->links() }}
