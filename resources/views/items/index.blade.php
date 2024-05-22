c5fea03db88068748ba92b5e8532a2e5788ed600
hirano-takayuki-6547 <429-student005@la-study.com>
2024-05-22 14:46:00 +0900

    fixed header

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,23 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="search-category">
-
-        <div class="search-box">
-            <form action="{{ route('items.index') }}" method="get">
-                @csrf
-                <select class="category">
-                    <option value="#" disabled selected>カテゴリー</option>
-                    @foreach ($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                    @endforeach
-                </select>
-                <input class="search-txt" type="text" name="keyword" value="" placeholder="検索したいことを入力">
-                <button class="search-btn" type="submit">検索</button>
-            </form>
-        </div>
-    </div>
-
     @foreach ($items as $item)
         <article class="card">
             <div class="card__header">
@@ -37,6 +20,5 @@
             </div>
         </article>
     @endforeach
-
     {{ $items->links() }}
 @endsection
