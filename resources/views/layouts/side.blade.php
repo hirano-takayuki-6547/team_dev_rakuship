149afa52c84c8fc09b3679741bddccd6f188b9d2
kim-jinhong-6724 <429-student017@la-study.com>
2024-05-23 14:20:33 +0900

    fix searchbar and mypage

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100..900&family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/main/side-menu.css">
-    <link rel="stylesheet" href="/main/mypage-profile.css">
 
 </head>
 <body>
+
     <div class="container">
         <aside class="sidebar">
             <div class="brand-img">
@@ -18,19 +22,20 @@
             </div>
             <nav>
                 <ul>
-                    <li><a href="{{ route('items.index') }}">ホームへ</a></li>
-                    <li><a href="{{ route('mypage.index') }}">マイページ</a></li>
-                    <li><a href="{{ route('mypage.liked-items') }}">いいね！一覧</a></li>
-                    <li><a href="{{ route('mypage.bought-items') }}">購入商品一覧</a></li>
-                    <li><a href="{{ route('mypage.sold-items') }}">出品商品一覧</a></li>
+                    <li><a href="{{ route('items.index') }}"><i class="fa fa-home"></i> ホームへ</a></li>
+                    <li><a href="{{ route('mypage.index') }}"><i class="fa fa-wpforms"></i> マイページ</a></li>
+                    <li><a href="{{ route('mypage.liked-items') }}"><i class="fa fa-heart"></i> いいね！一覧</a></li>
+                    <li><a href="{{ route('mypage.bought-items') }}"><i class="fa fa-cubes"></i> 購入商品一覧</a></li>
+                    <li><a href="{{ route('mypage.sold-items') }}"><i class="fa fa-list"></i> 出品商品一覧</a></li>
                 </ul>
             </nav>
         </aside>
 
-        <main class="main-content">
+        <div class="main-content">
+            <h2 class="grow">@yield('title')</h2>
             @yield('content-mypage')
-        </main>
-
+        </div>
     </div>
+
 </body>
 </html>
--- resources/views/mypage/sold-items.blade.php
+++ resources/views/mypage/sold-items.blade.php
@@ -1,24 +1,11 @@
 @extends('layouts.side')
 
 @section('content-mypage')
-<h3>出品一覧</h3>
-<table border="1">
-    <thead>
-        <tr><th>ID</th><th>商品名</th><th>注文日</th><th></th></tr>
 
-    </thead>
-    <tbody>
-        @foreach($items as $item)
 
-            <tr>
-                <td>{{ $item->id }}</td>
-                <td>{{ $item->name }}</td>
-                <td>{{ $item->updated_at }}</td>
-                <td><a href="{{ route('items.show', $item)}}">詳細</a></td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="card-warpper">
+
+</div>
 
 
 @endsection
