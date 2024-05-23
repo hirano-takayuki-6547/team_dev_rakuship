e716266855296af064c9b52ce01b515f125e1a02
kim-jinhong-6724 <429-student017@la-study.com>
2024-05-23 17:14:15 +0900

    fix mypage いいね、出荷一覧、購入商品　ーー＞修正必要

--- resources/views/mypage/bought-items.blade.php
+++ resources/views/mypage/bought-items.blade.php
@@ -2,5 +2,22 @@
 
 @section('content-mypage')
 
+@section('title', '購入商品一覧')
+
+<div class="row">
+@foreach ($items as $item)
+    <article class="card">
+        <a href="{{ route('items.show', $item->id) }}" style="text-decoration: none; color: #000000;">
+            <div class="card__header">
+                <figure class="card__thumbnail">
+                    <img src="/storage/item_images/{{ $item->img_src }}" alt="商品画像" class="card__image" style="object-fit: cover; width: 300px; height: 300px;">
+                </figure>
+                <h2 class="card__title">{{ $item->name }}</h2>
+            </div>
+        </a>
+    </article>
+@endforeach
+
+</div>
 
 @endsection
--- resources/views/mypage/sold-items.blade.php
+++ resources/views/mypage/sold-items.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.side')
 
 @section('content-mypage')
-    <h3>出品一覧</h3>
-    @foreach ($items as $item)
-        <div class="sold-item">
-            <a href="{{ route('items.show', $item->id) }}" style="text-decoration: none; color: #000000;">
-                <div class="sold-item">
-                    <img src="/storage/item_images/{{ $item->img_src }}" alt="商品画像"
-                        style="object-fit: cover; width: 200px; height: 200px;">
-                    <h2>{{ $item->name }}</h2>
-                </div>
-            </a>
-        </div>
-    @endforeach
+
+@section('title', '出品商品一覧')
+
+<div class="row">
+@foreach ($items as $item)
+    <article class="card">
+        <a href="{{ route('items.show', $item->id) }}" style="text-decoration: none; color: #000000;">
+            <div class="card__header">
+                <figure class="card__thumbnail">
+                    <img src="/storage/item_images/{{ $item->img_src }}" alt="商品画像" class="card__image" style="object-fit: cover; width: 300px; height: 300px;">
+                </figure>
+                <h2 class="card__title">{{ $item->name }}</h2>
+            </div>
+        </a>
+    </article>
+@endforeach
+</div>
+
+
 @endsection
