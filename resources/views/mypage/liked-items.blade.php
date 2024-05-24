ed091fa78cc703004c0bfdcf0bf999f1176ac8ef
kim-jinhong-6724 <429-student017@la-study.com>
2024-05-24 10:49:36 +0900

    mypage 修正・toppage navbar fixed・update font

--- resources/views/mypage/liked-items.blade.php
+++ resources/views/mypage/liked-items.blade.php
@@ -6,20 +6,22 @@
 @section('title', 'お気に入り一覧')
 
 <div class="row">
+    <div class="grid-wrapper">
 
     @foreach ($items as $item)
     <article class="card">
         <a href="{{ route('items.show', $item->id) }}" style="text-decoration: none; color: #000000;">
             <div class="card__header">
                 <figure class="card__thumbnail">
-                    <img src="/storage/item_images/{{ $item->img_src }}" alt="商品画像" class="card__image" style="object-fit: cover; width: 300px; height: 300px;">
+                    <img src="/storage/item_images/{{ $item->img_src }}" alt="商品画像" class="card__image">
                 </figure>
                 <h2 class="card__title">{{ $item->name }}</h2>
             </div>
         </a>
     </article>
+    @endforeach
 
-@endforeach
+    </div>
 </div>
 
 @endsection
