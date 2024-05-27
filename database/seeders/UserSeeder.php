f53e204e7aa5c96b659eb093717713abbc196d47
suzuki-emika-6923 <429-student018@la-study.com>
2024-05-27 10:13:41 +0900

    ユーザー情報を作成しました

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,16 @@ class UserSeeder extends Seeder
     public function run()
     {
        $data = [
-           ['name' => 'testuser', 'email' => 'testuser@example.com', 'password' => Hash::make('test0520')]
+        ['name' => '山田一郎', 'email' => 'yamada01@example.com', 'password' => Hash::make('yamada01')],
+        ['name' => '相川克典', 'email' => 'aikawa02@example.com', 'password' => Hash::make('aikawa02')],
+        ['name' => '望月瞳', 'email' => 'motiduki03@example.com', 'password' => Hash::make('motiduki03')],
+        ['name' => '永田奈美', 'email' => 'nagata04@example.com', 'password' => Hash::make('nagata04')],
+        ['name' => '星野昇', 'email' => 'hosino05@example.com', 'password' => Hash::make('hosino05')],
+        ['name' => '清水愛', 'email' => 'simizu06@example.com', 'password' => Hash::make('simizu06')],
+        ['name' => '吉川秀樹', 'email' => 'yosikawa07@example.com', 'password' => Hash::make('yosikawa07')],
+        ['name' => '小山綾子', 'email' => 'koyama08@example.com', 'password' => Hash::make('koyama08')],
+        ['name' => '内田真央', 'email' => 'utida09@example.com', 'password' => Hash::make('utida09')],
+        ['name' => '佐々木雄太', 'email' => 'sasaki10@example.com', 'password' => Hash::make('sasaki10')]
        ];
        \DB::table('users')->insert($data);
     }
